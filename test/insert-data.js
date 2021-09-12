3355148bb9d2cf3c1c37669a5f0d8026fb627cd1
root <root@DESKTOP-HVCKQU5.localdomain>
2021-09-12 13:50:08 -0300

    refatoring code usingo components

--- test/insert-data.js
+++ test/insert-data.js
@@ -47,3 +47,16 @@ new Vue({
       }
     }
   });
+
+
+
+// Component example
+// Definindo novo componente chamado button-counter
+Vue.component('button-counter', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">Você clicou em mim {{ count }} vezes.</button>'
+  })
