2415c001f6adcca84b2b5c3402054bd6796baaa6
root <root@DESKTOP-HVCKQU5.localdomain>
2021-09-12 12:12:42 -0300

    finish html implementation, now lets start vue.js

--- js/index.js
+++ js/index.js
@@ -3,30 +3,40 @@ $(document).ready(function() {
      * Add evento de click
      */
     function onAdd() {
-          var $ul, li, $li, $label, $div, livro;
-          livro = $('.js-novo-livro').val();
-
-          // valida se “livro” está vazio
-          if (livro === '') {
-                 return;
-          }
-
-          $ul = $('ul');
-          $li = $('<li>').appendTo($ul);
-          $div = $('<div>')
-                 .addClass('checkbox')
-                 .appendTo($li);
-
-          $label = $('<label>').appendTo($div);
-          $('<input>')
-                 .attr('type', 'checkbox')
-                 .addClass('js-livro')
-                 .attr('name', 'list')
-                 .click(toggleRemovido)
-                 .appendTo($label);
-
-          $label.append(livro);
-          $('.js-novo-livro').val('');
+        var $ul, li, $li, $label, $div, livro, autor;
+        livro = $('.js-novo-livro').val();
+        autor = $('.js-novo-autor').val();
+    
+        // valida se “livro” está vazio
+        if (livro === '') {
+              return;
+        }
+    
+        $ul = $('ul');
+        $li = $('<li>').appendTo($ul);
+        $div = $('<div>')
+              .addClass('checkbox')
+              .appendTo($li);
+    
+        $label = $('<label>').appendTo($div);
+        $('<input>')
+              .attr('type', 'checkbox')
+              .addClass('js-livro')
+              .attr('name', 'list')
+              .click(toggleRemovido)
+              .appendTo($label);
+    
+        $('<big>')
+              .appendTo($label)
+              .append(livro);
+    
+        $label.append(" - ");
+    
+        $('<small>')
+              .appendTo($label)
+              .append(autor);
+    
+        $('.js-novo-livro, .js-novo-autor').val('');
     }
 
     /**
